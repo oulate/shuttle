d4b7d1cf97821b08979d61c1e4708ee352172e19
za-chenzhenyuan <jason@chenzhenyuan.com>
2019-11-29 18:37:36 +0800

    优化工程

--- ladder.sh
+++ ladder.sh
@@ -5,6 +5,6 @@
 # Usage:
 #   docker rm $(docker stop ladder-x1)
 #   docker build --rm -f "Dockerfile" -t oulate/ladder:latest . && docker image prune
-#   docker run -it --restart="always" --env="PASSWORD=foo" --name="ladder-x1" -p 51300:51300/tcp -p 51300:51300/udp "xrosy/ladder:latest"
+#   docker run -itd --restart="always" --env="PASSWORD=vps666888" --name="ladder-x1" -p 51301:51300/tcp -p 51301:51300/udp "oulate/ladder:latest"
 #   docker exec -it ladder-x1 sh
 #   docker logs ladder-x1
